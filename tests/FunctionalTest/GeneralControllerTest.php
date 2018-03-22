6d2ba0a907e4c9dc919d3c496ea9adffefea59be
Ariel Belloc <abelloc@qubit.tv>
2018-03-22 17:14:34 -0300

    Se creó test para verificar la interacción entre módulos

---
+++ tests/FunctionalTest/GeneralControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\FunctionalTest;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class GeneralControllerTest extends WebTestCase
+{
+    public function testSomething()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(1, $crawler->filter('body:contains("Hello World")')->count());
+    }
+}
