12442f6fa9c76643f1a2ddd6fd376a69ebf03261
Ariel Belloc <abelloc@qubit.tv>
2018-04-19 16:47:23 -0300

    Se creó la clase para el form de Content

--- src/Core/RenderManager/Form/ContentType.php
+++ src/Core/RenderManager/Form/ContentType.php
@@ -1,11 +1,19 @@
 <?php
-/**
- * ContentType.php
- * @author  Ariel Belloc <abelloc@qubit.tv>
- * @license Propietary
- */
+namespace App\Core\RenderManager\Form;
 
-class ContentType
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ContentType extends AbstractType
 {
-
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('uuid', TextType::class)
+            ->add('description', TextType::class)
+            ->add('save', SubmitType::class)
+        ;
+    }
 }
